b26dca50ccb7a8e16f034f48db73be46d96a4448
Vinzzztty <kevinarnandes21@gmail.com>
2023-12-18 22:30:37 +0700

    add delete folder feature

--- main.py
+++ main.py
@@ -16,6 +16,14 @@ config = dotenv_values(".env")
 DOC_ID = config['DOC_ID']
 DOC_ID2 = ''
 
+def delete_download_folder(folder_path):
+    try:
+        # Check if the folder exists
+        if os.path.exists(folder_path):
+            # Remove the folder and its contents
+            shutil.rmtree(folder_path)
+    except Exception as e:
+        print(f"Error deleting folder: {str(e)}")
 
 @app.route('/api')
 def hello_world():
@@ -36,6 +44,9 @@ def recommend():
             outfit_data = json.load(f)
 
         rec = get_combinations(outfit_data)
+
+        # Delete the download folder after processing
+        delete_download_folder(FOLDER_DOWNLOAD_IMG)
 
         # Return a response, for example, a JSON response
         return jsonify(
